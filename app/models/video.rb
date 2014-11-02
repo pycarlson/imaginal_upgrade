faaef9062e47679b9ec77131f9b1f1cb6a5517c9
Adam Ducker <adam@ozarkwebdevelopers.com>
2014-11-02 06:41:24 -0600

    Takes control of the video embed and thumb back from the database and removes those columns from the table, this way we can control things better in the model and keep the DB smaller

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,7 +4,7 @@ class Video < ActiveRecord::Base
   has_many :ratings, :dependent => :destroy
   has_many :averages, :dependent => :destroy
 
-  before_save :parse_video_url, :get_video_thumbnail_link
+  #before_save :parse_video_url, :get_video_thumbnail_link
   validates :title, :description, :url, :presence => true
   validates :url, :title, uniqueness: true
   validates_length_of :description, :maximum => 250
@@ -28,28 +28,59 @@ class Video < ActiveRecord::Base
     self.overall_rating = self.overall_stars/self.overall_votes
     self.save
   end
-  
-  private
 
-  def parse_video_url
-    if (/vimeo.com/).match(self.url)
-      video_id = (/(\d+)($|\/)/.match(self.url)).to_s
-      self.link = "<iframe src='http://player.vimeo.com/video/#{video_id}?title=0&byline=0&portrait=0' width='900' height='500' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>"
-    elsif (/youtube.com/).match(self.url)
-      video_id = (/([\w-]{11})/.match(self.url)).to_s
-      self.link = "<iframe width='900' height='500' src='//www.youtube.com/embed/#{video_id}?modestbranding=1&showinfo=0&rel=0&hd=1&controls=0&wmode=opaque' frameborder='0' allowfullscreen></iframe>"
+  def get_video_type(url)
+    if (/vimeo.com/).match(url)
+      get_video_type = 'vimeo'
+     elsif (/youtube.com/).match(url)
+       get_video_type = 'youtube'
     end
   end
 
-  def get_video_thumbnail_link
-    if (/vimeo.com/).match(self.url)
-      video_id = (/(\d+)($|\/)/.match(self.url)).to_s
-      self.thumbnail_link = "https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?url=https%3A%2F%2Fres.cloudinary.com%2Fdemo%2Fimage%2Fvimeo%2F#{video_id}.jpg&container=focus&resize_w=210&resize_h=140&refresh=2592000"
-    elsif (/youtube.com/).match(self.url)
-      video_id = (/([\w-]{11})/.match(self.url)).to_s
-      self.thumbnail_link = "https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?url=https%3A%2F%2Fimg.youtube.com%2Fvi%2F#{video_id}%2F0.jpg&container=focus&resize_w=210&resize_h=140&refresh=2592000"
-      self.facebook_thumbnail_link = "http://i1.ytimg.com/vi/#{video_id}/hqdefault.jpg?feature=og"
+  def get_video_id(url)
+    if (/vimeo.com/).match(url)
+      get_video_id = (/(\d+)($|\/)/.match(self.url)).to_s
+    elsif (/youtube.com/).match(url)
+      get_video_id = (/([\w-]{11})/.match(self.url)).to_s
     end
+  end
+
+  def embed_code
+
+    video_type = self.get_video_type(self.url)
+    video_id = self.get_video_id(self.url)
+
+    if video_type == 'vimeo'
+      embed_code = "<iframe src='http://player.vimeo.com/video/#{video_id}?title=0&byline=0&portrait=0' width='900' height='500' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>"
+    elsif video_type == 'youtube'
+      embed_code = "<iframe width='900' height='500' src='//www.youtube.com/embed/#{video_id}?modestbranding=1&showinfo=0&rel=0&hd=1&controls=0&wmode=opaque' frameborder='0' allowfullscreen></iframe>"
+    end
+  end
+
+  def thumbnail
+
+    video_type = self.get_video_type(self.url)
+    video_id = self.get_video_id(self.url)
+
+    if video_type == 'vimeo'
+      thumbnail = "https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?url=https%3A%2F%2Fres.cloudinary.com%2Fdemo%2Fimage%2Fvimeo%2F#{video_id}.jpg&container=focus&resize_w=210&resize_h=140&refresh=2592000"
+    elsif video_type == 'youtube'
+      thumbnail = "https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?url=https%3A%2F%2Fimg.youtube.com%2Fvi%2F#{video_id}%2F0.jpg&container=focus&resize_w=210&resize_h=140&refresh=2592000"
+    end
+
+  end
+
+  def facebook_thumbnail
+
+    video_type = self.get_video_type(self.url)
+    video_id = self.get_video_id(self.url)
+
+    if video_type == 'youtube'
+      facebook_thumbnail = "http://i1.ytimg.com/vi/#{video_id}/hqdefault.jpg?feature=og"
+    else
+      facebook_thumbnail = ''
+    end
+
   end
 end
 
